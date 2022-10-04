989322c019b3abf0c81038900dbb2d9f8f277425
Precious <precious.akam@hotmail.com>
2022-10-04 15:39:26 +1030

    'Changes'

--- Form/models.py
+++ Form/models.py
@@ -1,9 +1,9 @@
-from .db import db
+# from .db import db
 
-class Form(db.Document):
-    first_name = db.StringField(required=True, unique=True)
-    last_name = db.StringField(required=True, unique=True)
-    email = db.StringField(required=True, unique=True)
-    phone = db.StringField(required=True, unique=True)
+# class Form(db.Document):
+#     first_name = db.StringField(required=True, unique=True)
+#     last_name = db.StringField(required=True, unique=True)
+#     email = db.StringField(required=True, unique=True)
+#     phone = db.StringField(required=True, unique=True)
 
     
