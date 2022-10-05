d50aa574eb895e3f5d4a2e5c647cf371c29ab1df
Precious <precious.akam@hotmail.com>
2022-10-05 12:20:51 +1030

    Dic retrieval

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from xml.sax.handler import DTDHandler
 from flask import Flask, jsonify, request, json
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
@@ -15,6 +16,8 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+#Loop Back interface
+
 
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
@@ -27,14 +30,30 @@ def webhook():
         request_json_dict = json.loads(request_json_str)
        # print the dictionary
         print(request_json_dict)
-        #retrieve the value of the key 'name'
+
+        #retrieve the value of the keys
         firstname = request_json_dict['billing']['first_name']
+        lastname = request_json_dict['billing']['last_name']
+        email = request_json_dict['billing']['email']
+        phone = request_json_dict['billing']['phone']
+        product_id = request_json_dict['line_items'][0]['product_id']
+        quantity = request_json_dict['line_items'][0]['quantity']
+        school_name = request_json_dict['meta_data'][0]['value']
+        
+
+       #Convert dictionary values to sql objects
+
+       #print out examples
+        print(firstname)
+        print(lastname)
+
+
         #another way of retrieving the value of the key 'first_name'
         firstname2 = request_json_dict.get('_links').get('billing').get('first_name')
-        lastname = request_json_dict['billing']['last_name']
-        product_id = request_json_dict['_links']['line_items']['product_id']
+        lastname2 = request_json_dict['billing']['last_name']
         
-        print (firstname)
+        
+       
         # firstname           = request_json.get(['Body']['first_name'])
         # lastname           = request_json.get(['Body']['last_name'])
         # email           = request_json.get(['Body']['email'])
