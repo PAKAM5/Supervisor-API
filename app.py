5a75934317002bf7d170b0dbb7512085d1b8d2e5
Precious <precious.akam@hotmail.com>
2022-10-05 17:05:57 +1030

    Changes

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from xml.sax.handler import DTDHandler
+# from xml.sax.handler import DTDHandler
+import os
+import json
+import hmac
+import hashlib
+import requests
 from flask import Flask, jsonify, request, json
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
@@ -9,61 +14,82 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # api = Api(app)
-# 
+
+# SUPERVISOR_SECRET= SUPERVISOR APP SECRET
+# WOOCOMMERCE_SECRET= WOOCOMMERCE webhook secret>
+WOOCOMMERCE_SECRET = 'rUOE`&W$~/gaHj]{x{^l`=^ 7yLcn`L153Up=hiS<0;<7&_zA@'
+SUPERVISOR_SECRET = "lolo"
+
+
+SUPERVISOR_ENDPOINT = 'https://ops.irdo1.safecoms.net/webhook'
 
 
 @app.route('/', methods=['GET', "POST"])
 def index():
     return "Hello World!"
 
-#Loop Back interface
+#Loop Back interface----
 
 
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
-    if request.method == 'POST':
-        if request.headers['Content-Type'] == 'application/json':
-            request_json     = request.get_json()
-            #turn the json into a string
-            request_json_str = json.dumps(request_json)
-            #turn the string into a dictionary
-            request_json_dict = json.loads(request_json_str)
-        # print the dictionary
-            print(request_json_dict)
+    if request.method != 'POST':
+        return 405
+    if request.headers['Content-Type'] != 'application/json':
+        return 400
+    #Get Json
+    request_json     = request.get_json()
 
-            #retrieve the value of the keys
-            firstname = request_json_dict['billing']['first_name']
-            lastname = request_json_dict['billing']['last_name']
-            email = request_json_dict['billing']['email']
-            phone = request_json_dict['billing']['phone']
-            product_id = request_json_dict['line_items'][0]['product_id']
-            quantity = request_json_dict['line_items'][0]['quantity']
-            school_name = request_json_dict['meta_data'][0]['value']
-            
+    #turn the json into a string
+    request_json_str = json.dumps(request_json)
+    #turn the string into a dictionary
+    request_json_dict = json.loads(request_json_str)
+    # print the dictionary
+    print(request_json_dict)
 
-        #Convert dictionary values to sql objects
-
-        #print out examples
-            print(firstname)
-            print(lastname)
-
-
-            #another way of retrieving the value of the key 'first_name'
+    #retrieve the value of the keys
+    firstname = request_json_dict['billing']['first_name']
+    lastname = request_json_dict['billing']['last_name']
+    email = request_json_dict['billing']['email']
+    phone = request_json_dict['billing']['phone']
+    product_id = request_json_dict['line_items'][0]['product_id']
+    quantity = request_json_dict['line_items'][0]['quantity']
+    school_name = request_json_dict['meta_data'][0]['value']
+    
+    #tData to send to Application
         
-            
-            
+    send_data = (firstname, lastname, email, phone, product_id, quantity, school_name)
+
+    #Convert dictionary values to sql objects
+
+    #print out examples
+    print(firstname)
+    print(lastname)
+
+
+#another way of retrieving the value of the key 'first_name'
+
+    
+    # firstname           = request_json.get(['Body']['first_name'])
+    # lastname           = request_json.get(['Body']['last_name'])
+    # email           = request_json.get(['Body']['email'])
+    # phone           = request_json.get(['Body']['phone'])
+    # address_1       = request_json.get(['Body']['address_1'])
+    #print (result)
+    # return result
+    #print('JSON Message: ' + json.dumps(request.json))
+    print('First Name: ' + firstname)    
+
+    #if sending via json
+    header = {'content-type': 'application/json'}
+
+    #Send to supervisor app
+    Supervisor_response = requests.post(SUPERVISOR_ENDPOINT, 
+        send_data)
+
         
-            # firstname           = request_json.get(['Body']['first_name'])
-            # lastname           = request_json.get(['Body']['last_name'])
-            # email           = request_json.get(['Body']['email'])
-            # phone           = request_json.get(['Body']['phone'])
-            # address_1       = request_json.get(['Body']['address_1'])
-            #print (result)
-        # return result
-            #print('JSON Message: ' + json.dumps(request.json))
-            print('First Name: ' + firstname)    
+    
     return "Webhook received!"
-
 
 ###convert json to sql object
 #run app
