7d91ffcfa963213c01bb8ef29b1a9568b7ded13a
Precious <precious.akam@hotmail.com>
2022-10-19 19:41:35 +1030

    Check to see if count is working

--- app.py
+++ app.py
@@ -178,61 +178,91 @@ def webhook():
 
     except:
         raise DataInputError 
-    else: 
-        #Count number of records in query where school name is equal to school name in webhook
-        rec = School.query.filter_by(school_name = school_namew).count()
-        if rec >1 :
-            raise DataInputError
-            #if number of tuples is 1
-        elif rec== 1:
-            #subscription renewal 
-            #add subscription data - expiry date
-            if skuw == 'one-Month':
-                subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=30))
-                db.session.add(subscription_data)
-                db.session.commit()
-            elif skuw == 'one-year':
-                subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=365))
-                db.session.add(subscription_data)
-                db.session.commit()
-            #when data is sent to the database, convert user to is_superuser and is_approved
-            user = User()
-            user.is_superuser = True
-            user.is_approved = True
+    else:
+        #define random password generator function
+        school_data = School( school_name = school_namew)
+        db.session.add(school_data)
+        db.session.commit()
+        #INSERT INTO table subscription (school_id, expiry_date) VALUES ($schoolid, CURRENT_DATE + INTERVAL 1 MONTH);
+        #add subscription data
+        if skuw =='one-month':
+            subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=30))
+            db.session.add(subscription_data)
             db.session.commit()
-            return "Webhook received!"
-            #if number of tuples is 0
-        elif rec == 0: 
-            #define random password generator function
-            def random_password_generator(size=8, chars=string.ascii_letters + string.digits):
-                return ''.join(random.choice(chars) for _ in range(size))
-            #generate random password
-            random_password = random_password_generator()
-            school_data = School( school_name = school_namew)
-            db.session.add(school_data)
+        elif skuw =='one-year':
+            subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=365))
+            db.session.add(subscription_data)
             db.session.commit()
-           #INSERT INTO table subscription (school_id, expiry_date) VALUES ($schoolid, CURRENT_DATE + INTERVAL 1 MONTH);
-            #add subscription data
-            if skuw =='one-month':
-                subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=30))
-                db.session.add(subscription_data)
-                db.session.commit()
-            elif skuw =='one-year':
-                subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=365))
-                db.session.add(subscription_data)
-                db.session.commit()
+        
+        #add user data
+        user_data = User(first_name = first_namew, last_name = last_namew, email = emailw, phone = phonew, name = 'Administrator', password = 'hello100')
+        db.session.add(user_data)
+        db.session.commit()
+        
+        #when data is sent to the database, convert user to is_superuser and is_approved
+        user = User()
+        user.is_superuser = True
+        user.is_approved = True
+        db.session.commit()
+        return "Webhook received!"
+
+        # #Count number of records in query where school name is equal to school name in webhook
+        # rec = School.query.filter_by(school_name = school_namew).count()
+        # if rec >1 :
+        #     raise DataInputError
+        #     #if number of tuples is 1
+        # elif rec== 1:
+        #     #subscription renewal 
+        #     #add subscription data - expiry date
+        #     if skuw == 'one-Month':
+        #         subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=30))
+        #         db.session.add(subscription_data)
+        #         db.session.commit()
+        #     elif skuw == 'one-year':
+        #         subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=365))
+        #         db.session.add(subscription_data)
+        #         db.session.commit()
+        #     #when data is sent to the database, convert user to is_superuser and is_approved
+        #     user = User()
+        #     user.is_superuser = True
+        #     user.is_approved = True
+        #     db.session.commit()
+        #     return "Webhook received!"
+        #     #if number of tuples is 0
+        # elif rec == 0: 
+        #     #define random password generator function
+        #     def random_password_generator(size=8, chars=string.ascii_letters + string.digits):
+        #         return ''.join(random.choice(chars) for _ in range(size))
+        #     #generate random password
+        #     random_password = random_password_generator()
+        #     school_data = School( school_name = school_namew)
+        #     db.session.add(school_data)
+        #     db.session.commit()
+        #    #INSERT INTO table subscription (school_id, expiry_date) VALUES ($schoolid, CURRENT_DATE + INTERVAL 1 MONTH);
+        #     #add subscription data
+        #     if skuw =='one-month':
+        #         subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=30))
+        #         db.session.add(subscription_data)
+        #         db.session.commit()
+        #     elif skuw =='one-year':
+        #         subscription_data = Subscription(expiry_date = datetime.utcnow() + datetime.timedelta(days=365))
+        #         db.session.add(subscription_data)
+        #         db.session.commit()
            
-            #add user data
-            user_data = User(first_name = first_namew, last_name = last_namew, email = emailw, phone = phonew, name = 'Administrator', password = random_password)
-            db.session.add(user_data)
-            db.session.commit()
+        #     #add user data
+        #     user_data = User(first_name = first_namew, last_name = last_namew, email = emailw, phone = phonew, name = 'Administrator', password = random_password)
+        #     db.session.add(user_data)
+        #     db.session.commit()
             
-            #when data is sent to the database, convert user to is_superuser and is_approved
-            user = User()
-            user.is_superuser = True
-            user.is_approved = True
-            db.session.commit()
-            return "Webhook received!"
+        #     #when data is sent to the database, convert user to is_superuser and is_approved
+        #     user = User()
+        #     user.is_superuser = True
+        #     user.is_approved = True
+        #     db.session.commit()
+        #     return "Webhook received!"
+
+
+
 # #  #Create product subscription function for subscription by order table
 #     def subscription_duration():
 #         #monthly subscription
